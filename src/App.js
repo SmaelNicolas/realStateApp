acc0bec1a6e95ba9b889965d63d6f82230bf72b0
Smael Nicolas <nico.smael@gmail.com>
2022-07-22 18:09:30 -0300

    import components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import { Card } from "./Components/Card/Card";
+import { Navbar } from "./Components/Navbar/Navbar";
 
 function App() {
-	return <div className='app'></div>;
+	return (
+		<div className='app'>
+			{/* <Navbar /> */}
+			<Card />
+		</div>
+	);
 }
 
 export default App;
